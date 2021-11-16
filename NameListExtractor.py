2bcc8920c3605f127eed12a07af4e17dae51a8b4
NatCracken <natjmccracken@gmail.com>
2021-11-16 14:08:55 -0500

    Update NameListExtractor.py

--- NameListExtractor.py
+++ NameListExtractor.py
@@ -1,6 +1,6 @@
 from DbgPack import AssetManager
 
-assetsDir = r"E:\10-23-2021 Planetside 2 Resources" #replace this with the directoy of your .pack2 assets
+assetsDir = r"E:\10-23-2021 Planetside 2 Resources" #replace this with the directory of your .pack2 assets
 nameListFile = "NameList.txt"; #The namelist file
 
 print("Loading packs")
